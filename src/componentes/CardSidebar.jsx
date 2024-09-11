b651f3a0cb9f08fbfc0f84a738d7e83fe050bbe2
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-11 08:43:29 -0300

    React-Spotify

---
+++ src/componentes/CardSidebar.jsx
@@ -0,0 +1,5 @@
+export default function Card(){
+
+    return(<div className="bg-purple-400 w-32 h-32"></div>
+)
+}
---
+++ src/componentes/ConteudoPrincipal.jsx
@@ -0,0 +1,54 @@
+export default function ConteudoPrincipal(){
+
+    return(
+        <div className="bg-bege-400 w-3/4 grid grid-cols-4 place-itens-center items-center">
+          <div className="bg-purple-200 w-28 h-28 flex flex-col justify-around items-center">
+            <div className="bg-purple-400 w-3/4 h-7"></div>
+            <div className="bg-purple-400 w-3/4 h-7"></div>
+          </div>
+          <div className="bg-purple-200 w-28 h-28 flex flex-col justify-around items-center">
+            <div className="bg-purple-400 w-3/4 h-7"></div>
+            <div className="bg-purple-400 w-3/4 h-7"></div>
+          </div>
+          <div className="bg-purple-200 w-28 h-28 flex flex-col justify-around items-center">
+            <div className="bg-purple-400 w-3/4 h-7"></div>
+            <div className="bg-purple-400 w-3/4 h-7"></div>
+          </div>
+          <div className="bg-purple-200 w-28 h-28 flex flex-col justify-around items-center">
+            <div className="bg-purple-400 w-3/4 h-7"></div>
+            <div className="bg-purple-400 w-3/4 h-7"></div>
+          </div>
+          <div className="bg-purple-200 w-28 h-28 flex flex-col justify-around items-center">
+            <div className="bg-purple-400 w-3/4 h-7"></div>
+            <div className="bg-purple-400 w-3/4 h-7"></div>
+          </div>
+          <div className="bg-purple-200 w-28 h-28 flex flex-col justify-around items-center">
+            <div className="bg-purple-400 w-3/4 h-7"></div>
+            <div className="bg-purple-400 w-3/4 h-7"></div>
+          </div>
+          <div className="bg-purple-200 w-28 h-28 flex flex-col justify-around items-center">
+            <div className="bg-purple-400 w-3/4 h-7"></div>
+            <div className="bg-purple-400 w-3/4 h-7"></div>
+          </div>
+          <div className="bg-purple-200 w-28 h-28 flex flex-col justify-around items-center">
+            <div className="bg-purple-400 w-3/4 h-7"></div>
+            <div className="bg-purple-400 w-3/4 h-7"></div>
+          </div>
+          <div className="bg-purple-200 w-28 h-28 flex flex-col justify-around items-center">
+            <div className="bg-purple-400 w-3/4 h-7"></div>
+            <div className="bg-purple-400 w-3/4 h-7"></div>
+          </div>
+          <div className="bg-purple-200 w-28 h-28 flex flex-col justify-around items-center">
+            <div className="bg-purple-400 w-3/4 h-7"></div>
+            <div className="bg-purple-400 w-3/4 h-7"></div>
+          </div>
+          <div className="bg-purple-200 w-28 h-28 flex flex-col justify-around items-center">
+            <div className="bg-purple-400 w-3/4 h-7"></div>
+            <div className="bg-purple-400 w-3/4 h-7"></div>
+          </div>
+          <div className="bg-purple-200 w-28 h-28 flex flex-col justify-around items-center">
+            <div className="bg-purple-400 w-3/4 h-7"></div>
+            <div className="bg-purple-400 w-3/4 h-7"></div></div>  
+        </div>
+    )
+}
