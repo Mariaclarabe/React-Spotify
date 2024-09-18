d20bce8776af2f943e6cf25f8880a8f9fabbac62
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-18 08:43:45 -0300

    React-Spotify

---
+++ server/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import conectaNaDb from './db.js';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+const conexao = await conectaNaDb();
+
+conexao.on('error', (erro) => {
+    console.error('Erro ao conctar no MongoDB', erro)
+})
+
+conexao.once('open', () => {
+    console.log('Conectado ao MongoDB')
+})
+
+app.listen(3000, () => {
+    console.log('Servidor conectado')
+
+})
---
+++ server/db.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+export default async function conectaNaDb(){
+    mongoose.connect("mongodb+srv://maryaxzfal:mcfa*210806@cluster0.ijq4i.mongodb.net/Spotify?retryWrites=true&w=majority&appName=Cluster0")
+
+    return mongoose.connection;
+}
